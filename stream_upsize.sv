12143ab032277ee2d3ee1c6199754816bfb4fde1
Anatoly Alekseev <alekseev19988@gmail.com>
2024-05-19 16:30:24 +0400

    Адекватно работает запись сигнала keep и запись данных в FIFO. Необходимо доделать обработку чтения, а также доделать обработку сигнала last

--- stream_upsize.sv
+++ stream_upsize.sv
@@ -15,11 +15,12 @@ module stream_upsize #(
   input  logic                    m_ready_i
 );
 
-logic [T_DATA_RATIO-1:0] push_data_for_fifo, fifo_full, counter, pop_data_for_fifo, fifo_empty;
+logic [T_DATA_RATIO-1:0] push_data_for_fifo, fifo_full, counter_trn_reg, counter_trn, pointer, pop_data_for_fifo, fifo_empty;
 logic [T_DATA_WIDTH-1:0] data_fifo [T_DATA_RATIO-1:0];
-logic trn_vld;
+logic [T_DATA_RATIO-1:0] m_keep_o_logic;
+logic trn_vld, push_keep, pop_keep, empty_keep, full_keep;
 
-//Implement FIFO modules for project
+//Implement FIFO modules for save data
 generate
 for(genvar i = 0; i < T_DATA_RATIO; i++) begin : flip_flop_fifo
   flip_flop_fifo #(T_DATA_WIDTH, 10) flip_flop_fifo (
@@ -34,87 +35,120 @@ for(genvar i = 0; i < T_DATA_RATIO; i++) begin : flip_flop_fifo
   );
 end
 
+
+
 for(genvar fifo_idx = 0; fifo_idx < T_DATA_RATIO; fifo_idx++) begin : fifo_matrix
   assign m_data_o[fifo_idx] = data_fifo[fifo_idx];
 end
 endgenerate
 
+  flip_flop_fifo #(T_DATA_RATIO, 10) m_keep_fifo (
+    .clk(clk),
+    .rst(~rst_n),
+    .push(push_keep),
+    .pop(pop_keep),
+    .write_data(m_keep_o_logic),
+    .read_data(m_keep_o),
+    .empty(empty_keep),
+    .full(full_keep)  
+  );
 
 
 always_comb begin
   if (s_valid_i && s_ready_o)
-    push_data_for_fifo = (1'b1 << counter);
+    push_data_for_fifo = (1'b1 << pointer);
+
+  if ((counter_trn_reg == T_DATA_RATIO) && ~s_last_i && ~full_keep)
+    push_keep = 1'b1;
+  else
+    push_keep = '0;
+
+  if (counter_trn_reg == T_DATA_RATIO)
+    s_ready_o =   '0;
+  else
+    s_ready_o = 1'b1;
+
+    
 end
 
-   enum logic[2:0]
-   {
-      IDLE_VLD_DATA = 3'b000,
-      TRANSFER_STATE = 3'b001
-   }
-   state, new_state;
+  // enum logic[2:0]
+  //   {
+  //      IDLE_VLD_DATA = 3'b000,
+  //      TRANSFER_STATE = 3'b001
+  //   }
+  //   state, new_state;
 
 
 
- always_comb
-   begin
-     new_state = state;
+  // always_comb
+  //   begin
+  //     new_state = state;
 
-     case (state)
-       IDLE_VLD_DATA:
-       begin
-        m_valid_o = '0;
-        s_ready_o = 1;
-        if (counter == T_DATA_RATIO - 1) begin
-          s_ready_o = '0;
-          trn_vld = '0;
-          new_state = TRANSFER_STATE;
-        end else if (s_valid_i && s_ready_o && ~s_last_i) begin
-          trn_vld = 1;
-        end
-        end
-       TRANSFER_STATE:
-       begin
-         s_ready_o = '0;   
-         //pop_data_for_fifo = m_keep_o;
-         m_valid_o = 1'b1;
-         new_state = IDLE_VLD_DATA;
-       end
-//       F0:   
-//         if (  a) new_state = S1;
-//         else     new_state = IDLE;
-//       S1:   
-//         if (~ a) new_state = S0;
-//             else     new_state = F1;
-//       S0:   if (  a) new_state = S1;
-//             else     new_state = IDLE;
-     endcase
-   end
+  //     case (state)
+  //       IDLE_VLD_DATA:
+  //       begin
+  //         if (pointer )
+  //        end
+//        TRANSFER_STATE:
+//        begin
+//          s_ready_o = '0;   
+//          //pop_data_for_fifo = m_keep_o;
+//          m_valid_o = 1'b1;
+//          new_state = IDLE_VLD_DATA;
+//        end
+// //       F0:   
+// //         if (  a) new_state = S1;
+// //         else     new_state = IDLE;
+// //       S1:   
+// //         if (~ a) new_state = S0;
+// //             else     new_state = F1;
+// //       S0:   if (  a) new_state = S1;
+// //             else     new_state = IDLE;
+//      endcase
+//    end
 
-  // Output logic (depends only on the current state)
-  // assign detected = (state == S0);
+//   // Output logic (depends only on the current state)
+//   // assign detected = (state == S0);
 
-   // State update
-   always_ff @ (posedge clk)
-     if (~rst_n)
-       state <= IDLE_VLD_DATA;
-     else
-       state <= new_state;
+    // State update
+    // always_ff @ (posedge clk)
+    //   if (~rst_n)
+    //     state <= IDLE_VLD_DATA;
+    //   else
+    //     state <= new_state;
 
 
  always_ff @ (posedge clk) begin
-    if (~rst_n || s_last_i)
-      counter <= 0;
-    else if (s_valid_i && s_ready_o)
-      if (counter < T_DATA_RATIO - 1)
-        counter <= counter + 1;
-      else
-        counter <= 0;
-    if (trn_vld)  
-      m_keep_o <= m_keep_o | push_data_for_fifo;
-    else begin
-      pop_data_for_fifo <= m_keep_o;
-      m_keep_o <= '0;
+    if (~rst_n) begin
+      counter_trn_reg   <= 0;
+      m_keep_o_logic    <= 0;
+      pointer           <= 0;
+    end else begin
+
+      if (s_valid_i && ~s_last_i) begin
+        if (counter_trn_reg < T_DATA_RATIO) begin
+          counter_trn_reg <= counter_trn_reg + 1'b1;
+          m_keep_o_logic <= m_keep_o_logic + push_data_for_fifo;
+        end else begin
+          counter_trn_reg <= 0;
+          m_keep_o_logic  <= 0;
+        end 
+      end
+
+
+      if (s_valid_i && s_ready_o && ~s_last_i) begin
+        if (pointer < T_DATA_RATIO - 1)
+          pointer <= pointer + 1;
+        else
+          pointer <= '0;
+      end
     end
+    // if (trn_vld)  
+    //   m_keep_o_logic <= m_keep_o_logic | push_data_for_fifo;
+    // else begin
+    //   pop_data_for_fifo <= m_keep_o_logic;
+    //   m_keep_o_logic <= '0;
+    // end
 end
 
 endmodule
